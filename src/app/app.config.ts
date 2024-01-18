9a5de5e00be0066d88c7c4e04cba99cf6c75c149
Kacper Gotkowski <kgotkowski3@gmail.com>
2024-01-18 18:32:10 +0100

    Update Ngrx. Install Ngrx router-store.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -8,6 +8,7 @@ import { authFeatureKey, authReducer } from './auth/store/reducers.store';
 import { provideHttpClient } from '@angular/common/http';
 import { provideEffects } from '@ngrx/effects';
 import * as authEffects from './auth/store/effects.store';
+import { provideRouterStore } from '@ngrx/router-store';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -17,11 +18,12 @@ export const appConfig: ApplicationConfig = {
     provideState(authFeatureKey, authReducer),
     provideEffects(authEffects),
     provideStoreDevtools({
-      maxAge: 25,
-      logOnly: !isDevMode(),
-      autoPause: true,
-      trace: false,
-      traceLimit: 75,
+        maxAge: 25,
+        logOnly: !isDevMode(),
+        autoPause: true,
+        trace: false,
+        traceLimit: 75,
     }),
-  ],
+    provideRouterStore()
+],
 };
