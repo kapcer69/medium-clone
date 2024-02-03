1c15df4e509003d5986529be4c57656ece828f01
Kacper Gotkowski <kgotkowski3@gmail.com>
2024-02-03 16:01:37 +0100

    Add a banner

--- src/app/global-feed/components/global-feed.component.ts
+++ src/app/global-feed/components/global-feed.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { BannerComponent } from '../../shared/components/banner/banner.component';
 import { FeedComponent } from '../../shared/components/feed/feed.component';
 
 @Component({
   selector: 'mc-global-feed',
   templateUrl: './global-feed.component.html',
   standalone: true,
-  imports: [FeedComponent],
+  imports: [FeedComponent, BannerComponent],
 })
 export class GlobalFeedComponent {
   apiUrl = '/articles';
---
+++ src/app/shared/components/banner/banner.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'mc-banner',
+  templateUrl: './banner.component.html',
+  standalone: true,
+})
+export class BannerComponent {}
