fc25be813f8589f894b1f4f7d675dbe20a39682c
Kacper Gotkowski <kgotkowski3@gmail.com>
2024-02-06 16:39:08 +0100

    Fix redirectAfterLogging in and redirectAfterRegister effects

--- src/app/auth/store/effects.store.ts
+++ src/app/auth/store/effects.store.ts
@@ -12,7 +12,7 @@ export const getCurrentUserEffect = createEffect(
   (
     actions$ = inject(Actions),
     authService = inject(AuthService),
-    persistenceService = inject(PersistenceService)
+    persistenceService = inject(PersistenceService),
   ) => {
     return actions$.pipe(
       ofType(authActions.getCurrentUser),
@@ -28,19 +28,19 @@ export const getCurrentUserEffect = createEffect(
           }),
           catchError(() => {
             return of(authActions.getCurrentUserFailure());
-          })
+          }),
         );
-      })
+      }),
     );
   },
-  { functional: true }
+  { functional: true },
 );
 
 export const registerEffect = createEffect(
   (
     actions$ = inject(Actions),
     authService = inject(AuthService),
-    persistenceService = inject(PersistenceService)
+    persistenceService = inject(PersistenceService),
   ) => {
     return actions$.pipe(
       ofType(authActions.register),
@@ -54,36 +54,36 @@ export const registerEffect = createEffect(
             return of(
               authActions.registerFailure({
                 errors: errorResponse.error.errors,
-              })
+              }),
             );
-          })
+          }),
         );
-      })
+      }),
     );
   },
-  { functional: true }
+  { functional: true },
 );
 
-const redirectAfterRegisterEffect = createEffect(
+export const redirectAfterRegisterEffect = createEffect(
   (actions$ = inject(Actions), router = inject(Router)) => {
     return actions$.pipe(
       ofType(authActions.registerSuccess),
       tap(() => {
         router.navigateByUrl('/');
-      })
+      }),
     );
   },
   {
     functional: true,
     dispatch: false,
-  }
+  },
 );
 
 export const loginEffect = createEffect(
   (
     actions$ = inject(Actions),
     authService = inject(AuthService),
-    persistenceService = inject(PersistenceService)
+    persistenceService = inject(PersistenceService),
   ) => {
     return actions$.pipe(
       ofType(authActions.login),
@@ -97,27 +97,27 @@ export const loginEffect = createEffect(
             return of(
               authActions.loginFailure({
                 errors: errorResponse.error.errors,
-              })
+              }),
             );
-          })
+          }),
         );
-      })
+      }),
     );
   },
-  { functional: true }
+  { functional: true },
 );
 
-const redirectAfterLoginEffect = createEffect(
+export const redirectAfterLoginEffect = createEffect(
   (actions$ = inject(Actions), router = inject(Router)) => {
     return actions$.pipe(
       ofType(authActions.loginSuccess),
       tap(() => {
         router.navigateByUrl('/');
-      })
+      }),
     );
   },
   {
     functional: true,
     dispatch: false,
-  }
+  },
 );
